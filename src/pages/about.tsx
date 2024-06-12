21864603fa03a5e7c45431d59d56d3e5c123fa7d
send4t <133424813+send4t@users.noreply.github.com>
2024-06-12 12:31:50 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -73,10 +73,10 @@ function About({
                </Text>
                <br />
                <Text>
-               A Polkadot Hungary alapító csapata szakértőkből áll különböző területeken, a számítástechnikai fejlesztéstől, a marketingig. Ez a heterogén környezet lehetővé teszi számunkra, hogy hasznos és élvezetes tartalmakat állítsunk elő.
+               A Polkadot Hungary alapító csapata különböző területeken dolgozó szakértőkből áll, a számítástechnikai fejlesztők, marketingesek és web3 natív felhasználók alkotják. Ez a heterogén környezet lehetővé teszi számunkra, hogy hasznos és élvezetes tartalmakat állítsunk elő.
                </Text>
                < br />
-               <Text>Jelenleg követheted a Polkadot Hungary-t itt:</Text>
+               <Text>Jelenleg a Polkadot Hungary-t ezeken a felületen érheted el:</Text>
                   <UnorderedList>
                      <ListItem>
                          Twitter csatorna:{' '}
@@ -88,7 +88,16 @@ function About({
                                @PolkadotHungary
                                </a>
                      </ListItem>
-                     <ListItem>Blog: itt</ListItem>
+                      <ListItem>
+                           Telegram csatorna:{' '}
+                              <a
+                               href="https://t.me/polkadothungary"
+                               target="_blank"
+                                 rel="noopener noreferrer"
+                              >
+                               @polkadothungary
+                               </a>
+                     </ListItem>
                      </UnorderedList>
                      <br />
 
@@ -96,20 +105,20 @@ function About({
                   Részt venni a Polkadot Hungary-ben
                </Heading>
                <Text>
-                  Jelenleg a több módja is van annak, hogy támogasd a Polkadot Hungary-t, hogy kövess minket a Twitteren, és retweeteld a posztjainkat.
+                  Jelenleg a több módja is van annak, hogy támogasd a Polkadot Hungary-t.
                   Vegyél részt a havi eseményeinken. Stake-elj velünk és bízd ránk a szavazati erődet.
                </Text>
                <br />
                <Text>
-                  Bárki beküldhet egy eredeti cikket vagy fordítást a közzétételre a szerkesztőség döntése alapján. Jelenleg nem tudunk semmilyen juttatást garantálni, mivel a projekt még a kezdeti szakaszban van. Hamarosan tervezzük a Polkadot és a Kusama Tip-ek javaslatát értékes tartalmakra, amelyek nem a szerkesztőségből származnak, és a jövőben lehet, hogy egy on-chain jutalmat is kiosztunk.
+                  Terveink között szerepel, hogy bárki küldhessen nekünk eredeti cikket vagy fordítást és a szerkesztőség döntése alapján publikáljuk ezeket. Jelenleg nem tudunk semmilyen juttatást garantálni, mivel a projekt még a kezdeti szakaszban van és önkéntes alapon működik. Jövőben lehet, hogy on-chain jutalmakat is kiosztunk közösséget építő tagjaink számára.
                </Text>
                <Heading as="h2" fontSize="xl" pb={6}>
                <br />
                      Hogyan készült ez az oldal
                </Heading>
                <Text>
-                  A Polkadot szellemisége megkívánja, hogy ahol csak lehet decentralizált technológiát használjunk ezért nemcsak szavakban, hanem tettekben is igyekszünk így építkezni. A blogunk a web3 technológiákon alapul, az alap vázat a Polkadot Arena weboldaláról az engedélyükkel vettük át.
-                Minden cikkünk az IPFS protokollon keresztül van regisztrálva a <b>Polkaverse</b>-en keresztül, amely a Polkadot <a href="https://polkaverse.com/" target="_blank" rel="noopener noreferrer">Parachain projektje</a>.
+                  A Polkadot szellemisége megkívánja, hogy ahol csak lehet decentralizált technológiát használjunk ezért nemcsak elméletben, hanem tettekben is igyekszünk így építkezni. A blogunk a web3 technológiákon alapul, az alap vázat a Polkadot Arena weboldaláról az engedélyükkel vettük át.
+                Minden cikkünk az IPFS protokollon keresztül van regisztrálva a <b>Grill app</b>-on, amely a Polkadot <a href="https://grillapp.net" target="_blank" rel="noopener noreferrer">Parachain projektje</a>.
                </Text>
                <br />
                <Text>
@@ -117,8 +126,8 @@ function About({
                </Text>
                <br />
                <Text>
-                  A blogunk jelenleg az első verziójában van, és kiemelt fontosságú számunkra a jó SEO és a helyes cikkek on-chain történő visszakeresése.
-                  Folyamatosan dolgozunk, hogy egy jobb és a még felhasználóbarátabb tájékoztatást nyújtsunk .
+                  A blogunk jelenleg az első verziójában van, és kiemelt fontosságú számunkra a releváns cikkek megjelenítése magyar nyelven.
+                  Folyamatosan dolgozunk, hogy jobb és a még felhasználóbarátabb tájékoztatást nyújtsunk .
                </Text>
             </GridItem>
             <GridItem
@@ -129,7 +138,7 @@ function About({
                display="flex"
                justifyContent="center"
                alignItems="center"
-               minHeight="200px" // Adjust the height as needed
+               minHeight="200px" 
 >
             <Box>
             <Image src='polkadotHU.png' alt='Polkadot logo' /> 
