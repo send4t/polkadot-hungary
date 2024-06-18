6293fe8f047f8452c175715b0a648ebbf31633cf
send4t <133424813+send4t@users.noreply.github.com>
2024-06-18 14:43:44 +0200

    i

--- src/graphql/query/polkadot/index.ts
+++ src/graphql/query/polkadot/index.ts
@@ -1,31 +1,23 @@
-import { gql } from '@apollo/client';
-import { allSpaces } from '../../whitelist';
-import { filterIds, spaceData } from '../utils';
-import { graphqlQuery } from '../query';
+import { gql } from '@apollo/client'
+import { allSpaces } from '../../whitelist'
+import { filterIds, spaceData } from '../utils'
+import { graphqlQuery } from '../query'
 
 export const polkadotPageQuery = async () => {
    return await graphqlQuery({
       query: gql`
          ${showPolkadotFeed()}
       `,
-   });
-};
+   })
+}
 
 export function showPolkadotFeed() {
    return `query MyQuery {
-      posts(
-         where: {
-            OR: [
-               { tagsOriginal_contains: "Polkadot" },
-               { title_contains: "Polkadot" }
-            ],
-            AND: { space_in: ${JSON.stringify(filterIds(allSpaces))} },
-            kind_eq: RegularPost,
-            hidden_eq: false
-         },
-         orderBy: createdAtTime_DESC
-      ) {
-         ${spaceData()}
+      posts(where: {tagsOriginal_contains: "Polkadot", AND: {space: ${filterIds(
+         allSpaces
+      )}}, kind_eq: RegularPost, hidden_eq: false}, orderBy: createdAtTime_DESC) {
+        ${spaceData()}    
       }
-   }`;
+    }`
 }
+
