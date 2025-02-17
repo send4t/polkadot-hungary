9bddb86e0db37189607a4af2249ac918b252eabf
send4t <133424813+send4t@users.noreply.github.com>
2025-02-17 14:34:37 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -98,16 +98,19 @@ function Home({
 
     {/* Calendar Box */}
     <Box textAlign="right" mt={{ base: 6, md: 0 }}>
-      <iframe
-        src="https://lu.ma/embed/calendar/cal-7auh9QJGWDn9inH/events"
-        width="100%"
-        height="300"
-        frameBorder="0"
-        style={{ border: "1px solid #bfcbda88", borderRadius: "4px" }}
-        allowFullScreen
-        aria-hidden="false"
-        tabIndex={0}
-      ></iframe>
+     <iframe
+  src="https://lu.ma/embed/calendar/cal-7auh9QJGWDn9inH/events"
+  width="100%"
+  height={useBreakpointValue({ base: "300", md: "400" })}
+  frameBorder="0"
+  style={{
+    border: "1px solid #bfcbda88",
+    borderRadius: "4px",
+  }}
+  allowFullScreen
+  aria-hidden="false"
+  tabIndex={0}
+/>
     </Box>
   </SimpleGrid>
 
